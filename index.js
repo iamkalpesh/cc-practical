4ec184b2bcd4238b27e60edb0cde7fd7324b3853
iamkalpesh <kalpeshk_td2008@students.isquareit.edu.in>
2022-05-17 11:47:17 +0530

    added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 //to make pages .html .txt .jpeg .mp3 .mp4  static content 
 app.use(express.static("public"));
 
-app.listen(port || 5000, () => {
+app.listen(port, () => {
     console.log(`app started on port ${port}`);
 })
