49abd69a835ae256cd656723c1f7d6e8b9dc49d1
iamkalpesh <kalpeshk_td2008@students.isquareit.edu.in>
2022-05-17 11:39:21 +0530

    added

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const port = 4000;
 //to make pages .html .txt .jpeg .mp3 .mp4  static content 
 app.use(express.static("public"));
 
-app.listen(port, () => {
+app.listen(port || 5000, () => {
     console.log(`app started on port ${port}`);
 })
